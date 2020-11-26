b330741f63ccce28abd433ca9a975d25737c2a57
Vittorio Larovere <vlarovere@CLIFMI522.fritz.box>
2020-11-26 11:54:33 +0100

    MC-12 riorganizzazione progetto con AD

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import MorraCinese from './components/Custom/containers/MorraCinese/MorraCinese';
+import MorraCinese from './components/Custom/Pages/MorraCinese/MorraCinese';
 
 const App = () => <MorraCinese />;
 
