627c1b02a82606867624f7e666a6599e7ef6b907
Vittorio Larovere <vlarovere@CLIFMI522.fritz.box>
2020-11-26 12:36:47 +0100

    MC-12 riorganizzazione progetto per AD

--- src/components/Custom/Pages/PlayersPoints/PlayersPoints.tsx
+++ src/components/Custom/Pages/PlayersPoints/PlayersPoints.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import UserPointsLabel from '../../Atoms/PlayersPointsLabel/UserPointsLabel';
-import CalculatorPointsLabel from '../../Atoms/PlayersPointsLabel/CalculatorPointsLabel';
+import CalculatorPointsLabel from '../../Atoms/CalculatorPointsLabel/CalculatorPointsLabel';
 
 interface PlayerPointsProps {
   userPoints: number;
