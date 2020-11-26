676a7526d54d3df654fe8355a7e5be2a10b2503d
Vittorio Larovere <vlarovere@CLIFMI522.fritz.box>
2020-11-26 14:54:09 +0100

    MC-11 test funzionanti

--- src/test.tsx
+++ src/test.tsx
@@ -2,7 +2,7 @@ import { Label } from './components/Agnostic/Atoms/Label/Label';
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-test('Link changes the class when hovered', () => {
+test('Label has text', () => {
     const component = renderer.create(
       <Label value="testo di test" />,
     );
